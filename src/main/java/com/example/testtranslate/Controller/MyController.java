2096a89b7ff78fff71491c5d12e6bd4144aa4bc4
Simple-Jian <207628870@qq.com>
2023-12-13 23:36:43 +0800

    feat: 修修补补

--- src/main/java/com/example/testtranslate/Controller/MyController.java
+++ src/main/java/com/example/testtranslate/Controller/MyController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 对需要翻译的字段，先在数据库中指定一个除名字为"原字段名_en"以外其它完全相同的字段
@@ -43,7 +45,8 @@ public class MyController {
                     String origin_en=t.getString(fieldName+"_en");   //field content translated
 
                     boolean originIsBlank=origin==null?true:origin.isBlank();
-                    if(originIsBlank||origin_en!=null) return;          //if origin is null or has been translated, break this cycle
+                    boolean originEnIsBlank=origin_en==null?true:origin_en.isBlank();
+                    if(originIsBlank||!originEnIsBlank) return;          //if origin is null or has been translated, break this cycle
                     TransApi transApi=new TransApi(APP_ID,SECURITY_KEY);   //translate
                     String result="";
                     //If the request fails, try again until success.
@@ -64,5 +67,106 @@ public class MyController {
                 }
         );
        return test;
+    }
+
+    /**
+     * 用于翻译Oge_model_resource中的paramJson字段
+     * @return
+     */
+    @GetMapping("/translateParamJson")
+    public List<JSONObject> translateParamJson(){
+        String tName="oge_model_resource_param";
+        String fieldName="paramjson";
+        // 定义匹配连续中文字符的正则表达式
+        String regex = "\"[^\"]*[\u4e00-\u9fa5]+[^\"]*\"";
+        // 编译正则表达式
+        Pattern pattern = Pattern.compile(regex);
+
+        //tName是表名，fieldName是字段名
+        List<JSONObject> test = testMapper.getTest(tName);  //get all the fields content in the specified sheet in the database
+        test.forEach(
+                t->{
+                    String origin = t.getString(fieldName);  //get the field content
+//                    System.out.println(origin);
+                    String origin_en=t.getString(fieldName+"_en");   //field content translated
+
+                    boolean originIsBlank=origin==null?true:origin.isBlank();
+//                    boolean originEnIsBlank=origin_en==null?true:origin_en.isBlank();
+                    if(originIsBlank) return;          //if origin is null or has been translated, break this cycle
+
+                    TransApi transApi=new TransApi(APP_ID,SECURITY_KEY);   //translate
+                    //If the json contains chinese, translate it
+                    // 创建 Matcher 对象
+                    Matcher matcher = pattern.matcher(origin);
+                    // 循环匹配并替换每一处连续的中文字符
+                    StringBuffer resultBuffer = new StringBuffer();
+                    System.out.println("这个项目开始了："+t.getString("id"));
+                    while (matcher.find()) {
+                        // 获取匹配到的中文字符
+                        String chineseMatch = matcher.group();
+                        // 根据匹配到的中文字符进行不同的替换
+                        String result="";
+                        //If the request fails, try again until success.
+                        while (!result.contains("trans_result")){
+                            result = transApi.getTransResult(chineseMatch, "zh", "en");
+                            System.out.println("try translate items in paramJson..."+chineseMatch);
+                        }
+                        //parse the translated result
+                        JSONObject jsonObject = JSONObject.parseObject(result);
+                        JSONArray temp1 = jsonObject.getJSONArray("trans_result");
+                        JSONObject temp2 = temp1.getJSONObject(0);
+                        String translatedResult=temp2.getString("dst");
+                        System.out.println(translatedResult);
+                        // 替换并追加到结果字符串
+                        matcher.appendReplacement(resultBuffer, translatedResult);
+                    }
+                    if(!resultBuffer.isEmpty()) {
+                        // 将剩余的部分追加到结果字符串
+                        matcher.appendTail(resultBuffer);
+                        origin=resultBuffer.toString();
+                    }
+
+                    String id = t.getString("id");
+                    //update the field content with translated result
+                    testMapper.updateField(tName,fieldName+"_en",origin,Integer.parseInt(id));}
+        );
+        return test;
+}
+    @GetMapping("/translateScenes")
+    public List<JSONObject> translateScenes(String tName){
+        //tName是表名，fieldName是字段名
+        List<JSONObject> test = testMapper.getTest(tName);  //get all the fields content in the specified sheet in the database
+        test.forEach(
+                t->{
+
+                    String input =  t.getString("image_amount");;
+                    String id = t.getString("id");
+                    boolean inputIsBlank=input==null?true:input.isBlank();
+                    if(inputIsBlank) {
+                        System.out.println("input is blank!");
+                        return;
+                    };
+                    // 定义正则表达式，匹配数字后面紧跟着一个汉字的模式
+                    String pattern = "(\\d+)景";
+
+                    // 编译正则表达式
+                    Pattern regex = Pattern.compile(pattern);
+
+                    // 创建 Matcher 对象
+                    Matcher matcher = regex.matcher(input);
+
+                    // 查找匹配
+                    if (matcher.find()) {
+                        // 提取匹配到的数字部分
+                        String number = matcher.group(1);
+                        System.out.println("提取到的数字是: " + number);
+                        //update the field content with translated result
+                        testMapper.updateField(tName,"image_amount_en",number,Integer.parseInt(id));
+                    } else {
+                        System.out.println("未找到匹配的模式");
+                    }
+                }
+        );
+        return test;
     }
 }
--- src/test/java/com/example/testtranslate/TestTranslateApplicationTests.java
+++ src/test/java/com/example/testtranslate/TestTranslateApplicationTests.java
@@ -3,11 +3,51 @@ package com.example.testtranslate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @SpringBootTest
 class TestTranslateApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
-    }
+                // 测试字符串
+                String testString = "{\"url\":\"\",\"type\":\"\",\"inputs\":[{\"name\":\"cube\",\"type\":\"Cube\",\"description\":\"要可视化的Cube\"},{\"name\":\"products\",\"type\":\"List<String>\",\"description\":\"要可视化的产品列表\"},{\"name\":\"bands\",\"type\":\"List<String>\",\"description\":\"要映射到 RGB 的三个波段名称的逗号分隔列表\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"gain\",\"type\":\"Number/List<Number>\",\"description\":\"与每个像素值相乘的值,单个值或三个值的列表，每个波段一个\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"bias\",\"type\":\"Number/List<Number>\",\"description\":\"每个Dn值都相加的值,单个值或三个值的列表，每个波段一个\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"min\",\"type\":\"Number/List<Number>\",\"description\":\"映射到0的值,单个值或三个值的列表，每个波段一个\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"max\",\"type\":\"Number/List<Number>\",\"description\":\"映射到255的值,单个值或三个值的列表，每个波段一个\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"gamma\",\"type\":\"Number/List<Number>\",\"description\":\"Gamma校正系数,单个值或三个值的列表，每个波段一个\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"opacity\",\"type\":\"Float\",\"description\":\"图层的不透明度(0.0 为完全透明, 1.0 为完全不透明)\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"palette\",\"type\":\"List<String>\",\"description\":\"CSS 样式颜色字符串列表（仅限单波段图像）\",\"default\":\"None\",\"optional\":\"True\"},{\"name\":\"format\",\"type\":\"String\",\"description\":\"\",\"default\":\"png\",\"optional\":\"True\"}],\"outputs\":[{\"name\":\"Cube\",\"type\":\"Cube\",\"description\":\"可视化结果\"}]}";
+
+                // 定义匹配连续中文字符的正则表达式
+                String regex ="\"[^\"]*[\u4e00-\u9fa5]+[^\"]*\"";
+
+
+        // 编译正则表达式
+        Pattern pattern = Pattern.compile(regex);
+
+        // 创建 Matcher 对象
+        Matcher matcher = pattern.matcher(testString);
+
+        // 循环匹配并替换每一处连续的中文字符
+        StringBuffer resultBuffer = new StringBuffer();
+        int replacementCount = 1;
+
+        while (matcher.find()) {
+            // 获取匹配到的中文字符
+            String chineseMatch = matcher.group();
+
+            // 根据匹配到的中文字符进行不同的替换
+            String replacementText = "替换文本" + replacementCount;
+
+            // 替换并追加到结果字符串
+            matcher.appendReplacement(resultBuffer, replacementText);
+
+            // 更新替换计数
+            replacementCount++;
+        }
+
+        // 将剩余的部分追加到结果字符串
+        matcher.appendTail(resultBuffer);
+
+        // 输出结果
+        System.out.println("替换前：" + testString);
+        System.out.println("替换后：" + resultBuffer.toString());
+            }
 
 }
